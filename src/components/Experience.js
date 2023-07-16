bde4a2b8ee168ffd0c0dd772665074644a8802f9
Stefanie Engstrom <38473399+s3ngstrom@users.noreply.github.com>
2023-07-16 18:34:23 -0500

    Updating color theming for consistency

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,7 +6,7 @@ export default function Testimonials() {
   return (
     <section id="experience">
       <div className="container px-5 py-10 mx-auto text-center bg-gradient-to-b from-indigo-900 to-purple-600">
-        <UsersIcon className="w-10 inline-block mb-4" />
+        <UsersIcon className="text-pink-600 w-10 inline-block mb-4" />
         <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
           Professional Experience
         </h1>
@@ -14,7 +14,7 @@ export default function Testimonials() {
           {jobs.map((job) => (
             <div className="p-4 md:w-1/2 w-full">
               <div className="h-full bg-gray-800 bg-opacity-40 p-8 rounded">
-                <TerminalIcon className="block w-8 text-gray-500 mb-4" />
+                <TerminalIcon className="text-pink-600 block w-8 text-gray-500 mb-4" />
                 <p className="leading-relaxed mb-6">{job.quote}</p>
                 <div className="inline-flex items-center">
                   <img

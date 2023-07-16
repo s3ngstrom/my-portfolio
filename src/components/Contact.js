40b3851955e86933bd0160c7d4c40f801de5c65a
Stefanie Engstrom <38473399+s3ngstrom@users.noreply.github.com>
2023-07-16 14:40:24 -0500

    Finalizing layout.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 export default function Contact() {
   const [name, setName] = React.useState("");
   const [email, setEmail] = React.useState("");
@@ -6,9 +7,7 @@ export default function Contact() {
 
   function encode(data) {
     return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
+      .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
       .join("&");
   }
 
@@ -25,98 +24,103 @@ export default function Contact() {
 
   return (
     <section id="contact" className="relative">
-      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-            <div className="lg:w-1/2 px-6">
-              
+      <div className="container px-5 py-10 mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+          <div className="bg-gray-900 rounded-lg overflow-hidden p-10 text-center">
+            <div className="mb-4">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 LOCATION
               </h2>
-              <p className="mt-1">
-                Chicago, Illinois
-              </p>
+              <p className="mt-1">Chicago, Illinois</p>
             </div>
-            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+            <div className="mb-4">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
-                sge.engstrom@email.com
-              </a>
+              <a className="text-indigo-400 leading-relaxed hover:text-indigo-200">sge.engstrom@email.com</a>
+            </div>
+            <div>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 LINKS
               </h2>
-              <p className="leading-relaxed">Github</p>
-              <p className="leading-relaxed">LinkedIn</p>
+              <a href="https://github.com/s3ngstrom" className="text-indigo-400 leading-relaxed hover:text-indigo-200">
+                Github
+              </a>
+              <p />
+              <a
+                href="https://linkedin.com/in/stefanie-engstrom"
+                className="text-indigo-400 leading-relaxed hover:text-indigo-200"
+              >
+                LinkedIn
+              </a>
             </div>
-            <img
+            <div className="text-center">
+          <img
             className="object-cover object-center rounded"
             alt="The Chicago Flag"
             src="./chicago.png"
             style={{
-              maxHeight: '400px',
-              maxWidth: '300px',
-              display: 'block',
-              margin: '0 auto',
-              paddingTop: '50px'
-            }}          />
-          </div>
+              maxHeight: "400px",
+              maxWidth: "300px",
+              display: "block",
+              margin: "0 auto",
+              paddingTop: "50px",
+            }}
+          />
         </div>
-        <form
-          netlify
-          name="contact"
-          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
-        >
-          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Hire Me
-          </h2>
-          <p className="leading-relaxed mb-5">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-            suscipit officia aspernatur veritatis. Asperiores, aliquid?
-          </p>
-          <div className="relative mb-4">
-            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
           </div>
-          <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-          <div className="relative mb-4">
-            <label
-              htmlFor="message"
-              className="leading-7 text-sm text-gray-400"
-            >
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-            />
-          </div>
-          <button
-            type="submit"
-            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          
+          <form
+            netlify
+            name="contact"
+            className="bg-gray-900 rounded-lg p-10"
           >
-            Submit
-          </button>
-        </form>
+            <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+              Hiring?
+            </h2>
+            <p className="leading-relaxed mb-5">
+              I'm based in Chicago and open to hybrid roles in the area, as well as remote roles anywhere.
+            </p>
+            <div className="relative mb-4">
+              <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div className="relative mb-4">
+              <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div className="relative mb-4">
+              <label htmlFor="message" className="leading-7 text-sm text-gray-400">
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <button
+              type="submit"
+              className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     </section>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+    return(
+        <div className="text-center py-4">
+            <h4 className="text-lg font-bold">Powered by</h4>
+            <p>Javascript | React | Tailwind</p>
+        </div>
+    );
+}

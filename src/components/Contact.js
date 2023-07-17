f38017657e4f954f83dd0a66e8530201b95ad45b
Stefanie Engstrom <38473399+s3ngstrom@users.noreply.github.com>
2023-07-17 00:42:33 -0500

    Troubleshooting for Netlify forms

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,9 @@ export default function Contact() {
 
   function encode(data) {
     return Object.keys(data)
-      .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
       .join("&");
   }
 
@@ -37,13 +39,18 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-purple-300 leading-relaxed hover:underline hover:text-purple-100">sge.engstrom@email.com</a>
+              <a className="text-purple-300 leading-relaxed hover:underline hover:text-purple-100">
+                sge.engstrom@email.com
+              </a>
             </div>
             <div>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 LINKS
               </h2>
-              <a href="https://github.com/s3ngstrom" className="text-purple-300 leading-relaxed hover:underline hover:text-purple-100">
+              <a
+                href="https://github.com/s3ngstrom"
+                className="text-purple-300 leading-relaxed hover:underline hover:text-purple-100"
+              >
                 Github
               </a>
               <p />
@@ -71,58 +78,64 @@ export default function Contact() {
           </div>
 
           <form
-  name="contact"
-  method="POST"
-  data-netlify="true"
-  className="bg-gray-900 rounded-lg p-10"
-  onSubmit={handleSubmit}
->
-  <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-    Hiring?
-  </h2>
-  <p className="leading-relaxed mb-5">
-    I'm based in Chicago and open to hybrid roles in the area, as well as remote roles anywhere.
-  </p>
-  <div className="relative mb-4">
-    <label htmlFor="name" className="leading-7 text-sm text-gray-300">
-      Name
-    </label>
-    <input
-      type="text"
-      id="name"
-      name="name"
-      className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-    />
-  </div>
-  <div className="relative mb-4">
-    <label htmlFor="email" className="leading-7 text-sm text-gray-300">
-      Email
-    </label>
-    <input
-      type="email"
-      id="email"
-      name="email"
-      className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-    />
-  </div>
-  <div className="relative mb-4">
-    <label htmlFor="message" className="leading-7 text-sm text-gray-300">
-      Message
-    </label>
-    <textarea
-      id="message"
-      name="message"
-      className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-    />
-  </div>
-  <button
-    type="submit"
-    className="text-white bg-purple-800 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded text-lg"
-  >
-    Submit
-  </button>
-</form>
-
+            name="contact"
+            method="POST"
+            data-netlify="true"
+            className="bg-gray-900 rounded-lg p-10"
+            onSubmit={handleSubmit}
+          >
+            <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+              Hiring?
+            </h2>
+            <p className="leading-relaxed mb-5">
+              I'm based in Chicago and open to hybrid roles in the area, as well
+              as remote roles anywhere.
+            </p>
+            <div className="relative mb-4">
+              <label htmlFor="name" className="leading-7 text-sm text-gray-300">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div className="relative mb-4">
+              <label
+                htmlFor="email"
+                className="leading-7 text-sm text-gray-300"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div className="relative mb-4">
+              <label
+                htmlFor="message"
+                className="leading-7 text-sm text-gray-300"
+              >
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <button
+              type="submit"
+              className="text-white bg-purple-800 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded text-lg"
+            >
+              Submit
+            </button>
+          </form>
         </div>
       </div>
     </section>

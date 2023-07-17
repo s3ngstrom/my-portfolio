4a62019128ded225270098678c75c55708202a02
Stefanie Engstrom <38473399+s3ngstrom@users.noreply.github.com>
2023-07-17 00:07:00 -0500

    Updating contact form to troubleshoot Netlify

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -70,52 +70,58 @@ export default function Contact() {
             </div>
           </div>
 
-          <form netlify name="contact" className="bg-gray-900 rounded-lg p-10">
-            <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-              Hiring?
-            </h2>
-            <p className="leading-relaxed mb-5">
-              I'm based in Chicago and open to hybrid roles in the area, as well as remote roles anywhere.
-            </p>
-            <div className="relative mb-4">
-              <label htmlFor="name" className="leading-7 text-sm text-gray-300">
-                Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <div className="relative mb-4">
-              <label htmlFor="email" className="leading-7 text-sm text-gray-300">
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <div className="relative mb-4">
-              <label htmlFor="message" className="leading-7 text-sm text-gray-300">
-                Message
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <button
-              type="submit"
-              className="text-white bg-purple-800 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded text-lg"
-            >
-              Submit
-            </button>
-          </form>
+          <form
+  name="contact"
+  method="POST"
+  data-netlify="true"
+  className="bg-gray-900 rounded-lg p-10"
+>
+  <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+    Hiring?
+  </h2>
+  <p className="leading-relaxed mb-5">
+    I'm based in Chicago and open to hybrid roles in the area, as well as remote roles anywhere.
+  </p>
+  <div className="relative mb-4">
+    <label htmlFor="name" className="leading-7 text-sm text-gray-300">
+      Name
+    </label>
+    <input
+      type="text"
+      id="name"
+      name="name"
+      className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+    />
+  </div>
+  <div className="relative mb-4">
+    <label htmlFor="email" className="leading-7 text-sm text-gray-300">
+      Email
+    </label>
+    <input
+      type="email"
+      id="email"
+      name="email"
+      className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+    />
+  </div>
+  <div className="relative mb-4">
+    <label htmlFor="message" className="leading-7 text-sm text-gray-300">
+      Message
+    </label>
+    <textarea
+      id="message"
+      name="message"
+      className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+    />
+  </div>
+  <button
+    type="submit"
+    className="text-white bg-purple-800 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded text-lg"
+  >
+    Submit
+  </button>
+</form>
+
         </div>
       </div>
     </section>

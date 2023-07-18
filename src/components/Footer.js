dbcfc9f64b5928af1acf1fc19b2b1f47e2644a84
Stefanie Engstrom <38473399+s3ngstrom@users.noreply.github.com>
2023-07-18 12:16:00 -0500

    Adding logos for technology

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,44 @@
 import React from "react";
 
 export default function Footer() {
-    return(
-        <div className="text-center py-4">
-            <h4 className="text-lg font-bold">Powered by</h4>
-            <p>Javascript | React | Tailwind</p>
-        </div>
-    );
+  return (
+    <div className="text-center py-4">
+      <h4 className="text-lg font-bold">Built With</h4>
+      <div className="flex items-center justify-center mt-2">
+        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noopener noreferrer">
+          <img
+            className="object-cover object-center rounded"
+            alt="JavaScript"
+            src="./javascript.png"
+            style={{
+              maxWidth: "50px",
+              display: "block",
+            }}
+          />
+        </a>
+        <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
+          <img
+            className="object-cover object-center rounded"
+            alt="React"
+            src="./react.svg"
+            style={{
+              maxWidth: "50px",
+              display: "block",
+            }}
+          />
+        </a>
+        <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer">
+          <img
+            className="object-cover object-center rounded"
+            alt="Tailwind CSS"
+            src="./tailwind.svg"
+            style={{
+              maxWidth: "50px",
+              display: "block",
+            }}
+          />
+        </a>
+      </div>
+    </div>
+  );
 }

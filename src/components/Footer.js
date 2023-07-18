5fbaec29a5452a31e711d03498904f3e152cc0d5
Stefanie Engstrom <38473399+s3ngstrom@users.noreply.github.com>
2023-07-18 12:34:31 -0500

    Corrected tech logos; image file restructuring

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,35 +5,49 @@ export default function Footer() {
     <div className="text-center py-4">
       <h4 className="text-lg font-bold">Built With</h4>
       <div className="flex items-center justify-center mt-2">
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img
             className="object-cover object-center rounded"
             alt="JavaScript"
             src="./javascript.png"
             style={{
-              maxWidth: "50px",
+              maxWidth: "30px",
               display: "block",
             }}
           />
         </a>
-        <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
+        <p>&nbsp;|&nbsp;</p>
+        <a
+          href="https://reactjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img
             className="object-cover object-center rounded"
             alt="React"
-            src="./react.svg"
+            src="./logo192.png"
             style={{
-              maxWidth: "50px",
+              maxWidth: "30px",
               display: "block",
             }}
           />
         </a>
-        <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer">
+        <p>&nbsp;|&nbsp;</p>
+        <a
+          href="https://tailwindcss.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img
             className="object-cover object-center rounded"
             alt="Tailwind CSS"
-            src="./tailwind.svg"
+            src="./tailwind.png"
             style={{
-              maxWidth: "50px",
+              maxWidth: "30px",
               display: "block",
             }}
           />

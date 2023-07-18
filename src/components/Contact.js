a9946cd524b4867c73a3f50b7ba4e6b694f6c3c0
Stefanie Engstrom <38473399+s3ngstrom@users.noreply.github.com>
2023-07-18 11:46:12 -0500

    Updated formatting for Contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,52 +26,66 @@ export default function Contact() {
 
   return (
     <section id="contact" className="relative">
-  <div className="container px-5 py-10 mx-auto">
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-      {/* Left Column */}
-      <div className="bg-gray-900 rounded-lg overflow-hidden p-10 text-center">
-        <div className="mb-4">
-        <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-              Are you hiring?
-            </h2>
-            <p>I'm based in Chicago and seeking hybrid roles locally, and remote roles anywhere.</p>
-            <p>Connect with me or send me an email!</p>
-            <br />
-          <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-            LOCATION
+      <div className="container px-5 py-10 mx-auto">
+        {/* Are You Hiring (Centered) */}
+        <div className="text-center mb-2">
+          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+            Are you hiring?
           </h2>
-          <p className="mt-1">Chicago, Illinois</p>
+          <p>I'm based in Chicago and seeking hybrid/remote roles locally, and remote roles anywhere.</p>
+          <p>Connect with me or send me an email!</p>
         </div>
-        <div className="mb-4">
-          <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-            EMAIL
-          </h2>
-          <a className="text-purple-300 leading-relaxed hover:underline hover:text-purple-100">
-            sge.engstrom@email.com
-          </a>
-        </div>
-        <div>
-          <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
-            LINKS
-          </h2>
-          <a
-            href="https://github.com/s3ngstrom"
-            className="text-purple-300 leading-relaxed hover:underline hover:text-purple-100"
-          >
-            Github
-          </a>
-          <p />
-          <a
-            href="https://linkedin.com/in/stefanie-engstrom"
-            className="text-purple-300 leading-relaxed hover:underline hover:text-purple-100"
-          >
-            LinkedIn
-          </a>
+
+        {/* Three Columns */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
+          {/* Left Column */}
+          <div className="bg-gray-900 rounded-lg overflow-hidden p-4 text-center">
+            <div className="mb-1">
+              <h2 className="title-font font-semibold text-white tracking-widest text-s">
+                LOCATION
+              </h2>
+              <p className="mt-1">Chicago, Illinois</p>
+            </div>
+          </div>
+
+          {/* Middle Column */}
+          <div className="bg-gray-900 rounded-lg overflow-hidden p-4 text-center">
+            <div className="mb-1">
+              <h2 className="title-font font-semibold text-white tracking-widest text-s">
+                EMAIL
+              </h2>
+              <a className="text-purple-300 leading-relaxed hover:underline hover:text-purple-100">
+                sge.engstrom@email.com
+              </a>
+            </div>
+          </div>
+
+          {/* Right Column */}
+          <div className="bg-gray-900 rounded-lg overflow-hidden p-4 text-center">
+            <div>
+              <h2 className="title-font font-semibold text-white tracking-widest text-s">
+                LINKS
+              </h2>
+              <a
+                href="https://github.com/s3ngstrom"
+                className="text-purple-300 leading-relaxed hover:underline hover:text-purple-100"
+              >
+                Github
+              </a>
+              <p />
+              <a
+                href="https://linkedin.com/in/stefanie-engstrom"
+                className="text-purple-300 leading-relaxed hover:underline hover:text-purple-100"
+              >
+                LinkedIn
+              </a>
+            </div>
+          </div>
         </div>
       </div>
 
-      {/* Right Column (with Chicago flag image) */}
-      <div className="flex items-center justify-center">
+      {/* Chicago Flag Image (Centered) */}
+      <div className="flex items-center justify-center mt-2">
         <img
           className="object-cover object-center rounded"
           alt="The Chicago Flag"
@@ -84,7 +98,6 @@ export default function Contact() {
           }}
         />
       </div>
-    </div>
-  </div>
-</section>
-)}
+    </section>
+  );
+}
